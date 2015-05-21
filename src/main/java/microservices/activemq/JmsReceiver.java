ab15585895569f32417c55c3757d5d108af57863
Jiakui Li <jiakui.li@booxware.de>
2015-05-21 12:35:21 +0200

    Add App Application.

--- src/main/java/microservices/activemq/JmsReceiver.java
+++ src/main/java/microservices/activemq/JmsReceiver.java
@@ -21,22 +21,26 @@ import org.apache.activemq.ActiveMQConnectionFactory;
 public class JmsReceiver {
 
 	public static void main(String[] args) throws JMSException {
+		
 		// ConnectionFactory
 		ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(
 				ActiveMQConnection.DEFAULT_USER,
 				ActiveMQConnection.DEFAULT_PASSWORD, "tcp://localhost:61616");
+		
 		// JMS Provider connection
 		Connection connection = connectionFactory.createConnection();
+		
 		// Session
 		Session session = connection.createSession(Boolean.FALSE,
 				Session.AUTO_ACKNOWLEDGE);
-		// Destination
-		// Get queue
+		
+		// Destination Get queue
 		Destination destination = session.createQueue("my-queue");
 
 		// Consumer
 		MessageConsumer consumer = session.createConsumer(destination);
 		connection.start();
+		
 		while (true) {
 			TextMessage message = (TextMessage) consumer.receive();
 			if (null != message) {
--- src/main/java/microservices/springboot/App.java
+++ src/main/java/microservices/springboot/App.java
@@ -4,6 +4,16 @@ package microservices.springboot;
  * Hello world!
  *
  */
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.MessageConsumer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+import org.apache.activemq.ActiveMQConnection;
+import org.apache.activemq.ActiveMQConnectionFactory;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.stereotype.*;
@@ -12,14 +22,56 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 @EnableAutoConfiguration
 public class App {
+	
+	private final static String LOCAL_HOST_TIP = "tcp://localhost:61616";
 
     @RequestMapping("/")
     @ResponseBody
     String home() {
-        return "Hello World!";
+        return getActiveMQQueue();
     }
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(App.class, args);
     }
+    
+    public static String getActiveMQQueue() {
+
+		// ConnectionFactory
+		ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(
+				ActiveMQConnection.DEFAULT_USER,
+				ActiveMQConnection.DEFAULT_PASSWORD, LOCAL_HOST_TIP);
+		
+		// JMS Provider connection
+		Connection connection;
+		try {
+			connection = connectionFactory.createConnection();
+
+			
+			// Session
+			Session session = connection.createSession(Boolean.FALSE,
+					Session.AUTO_ACKNOWLEDGE);
+			
+			// Destination Get queue
+			Destination destination = session.createQueue("my-queue");
+
+			// Consumer
+			MessageConsumer consumer = session.createConsumer(destination);
+			connection.start();
+			
+				TextMessage message = (TextMessage) consumer.receive();
+				if (null != message) {
+					String text = message.getText();
+					System.out.println("Message：" + message.getText());
+					session.close();
+					connection.close();
+					return text;
+				}
+		} catch (JMSException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return  "";
+
+	}
 }
